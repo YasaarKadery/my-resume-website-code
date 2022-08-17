5aed7dbdf77bb75bfbf73e09a2eb756b8e3142e8
YasaarKadery <ykadery12@gmail.com>
2022-08-17 17:35:18 -0400

    added comments

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 let numVisitors;
 var counterContainer = document.querySelector(".website-counter");
-
+/**
+ * Function that sends a PUT request to API gateway which then triggers a lambda function that
+ * updates an item in a dynamoDB table which keeps a keeps a counter of how many people have visited the page.
+ */
 const putData = async () => {
     const response = await fetch('https://8g17hv4jni.execute-api.us-east-1.amazonaws.com/items', 
     {
@@ -14,12 +17,12 @@ const putData = async () => {
    const data = await response.json();
    numVisitors = data['body']['num']['N']
    counterContainer.innerHTML = numVisitors;
-   console.log(numVisitors);
+   
 };  
 
 
 addEventListener('load', (event) => {
-    console.log('page is loaded');
+    //when the page loads, call this function
     putData();
     
 });
