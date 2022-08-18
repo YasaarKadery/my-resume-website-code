d257a05339c7357aefcc02658b36a1f9de17b0b7
YasaarKadery <90283249+YasaarKadery@users.noreply.github.com>
2022-08-18 14:30:42 -0400

    Add files via upload

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ var counterContainer = document.querySelector(".website-counter");
  * Function that sends a PUT request to API gateway which then triggers a lambda function that
  * updates an item in a dynamoDB table which keeps a keeps a counter of how many people have visited the page.
  */
+
 const putData = async () => {
     const response = await fetch('https://8g17hv4jni.execute-api.us-east-1.amazonaws.com/items', 
     {
@@ -18,14 +19,9 @@ const putData = async () => {
    numVisitors = data['body']['num']['N']
    counterContainer.innerHTML = numVisitors;
    
-};  
+};
+putData();
 
-
-addEventListener('load', (event) => {
-    //when the page loads, call this function
-    putData();
-    
-});
 
 
 
